cbbb53a8dd33bc75ae67e1918e9e2b3a9af375e4
Matt Leach <matt.leach25@gmail.com>
2015-08-08 17:29:44 -0700

    all extensions working

--- plot.py
+++ plot.py
@@ -1,35 +1,45 @@
 from api import get_commit_shas, get_commit, Commit
 from matplotlib import pyplot as plt
 import numpy as np
+from collections import Counter
+
+# Thanks http://rforwork.info/2012/05/18/bar-graph-colours-that-work-well/
+COLORS = ['#727272', '#f1595f', '#79c36a', '#599ad3', '#f9a65a', '#9e66ab', '#cd7058', '#d77fb3']
 
 
 def plot(username, repo):
     commits = get_commit_shas(username, repo)
-    py = []
-    html = []
+    loc_count = Counter()
+    ys = {}
     x = []
-
-    # Hard coding for now
-    py_loc = 0
-    html_loc = 0
 
     count = 0
     for sha in commits:
         count += 1
         c = Commit(get_commit(username, repo, sha))
         x.append(count)
-        py_loc += c.loc.get('py', 0)
-        html_loc += c.loc.get('html', 0)
-        html.append(html_loc)
-        py.append(py_loc)
 
-    y = np.row_stack((py, html))
+        # for each extension in the commit, add it to loc_count
+        for ext, val in c.loc.items():
+            loc_count[ext] += val
+            if ext not in ys:
+                ys[ext] = [0] * (count - 1)  # Fill with 0s
+
+        # For each extension we've seen append the current value to ys
+        for ext, val in loc_count.items():
+            ys[ext].append(val)
+
+    # stack ys appropriately
+    y = np.row_stack(ys.values())
     y_stack = np.cumsum(y, axis=0)
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
 
-    ax1.fill_between(x, 0, y_stack[0, :], facecolor='#ababab')
-    ax1.fill_between(x, y_stack[0, :], y_stack[1, :], facecolor='#12efef')
+    for val, ext in enumerate(ys.keys()):
+        if val == 0:
+            ax1.fill_between(x, 0, y_stack[0, :], facecolor=COLORS[val % len(COLORS)])
+        ax1.fill_between(x, y_stack[val-1, :], y_stack[val, :], facecolor=COLORS[val % len(COLORS)])
+        # TODO: legend
 
     plt.show()
 
